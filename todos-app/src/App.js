0e3b53ce556ff99ae040f7be6659a30c275424fd
Shamseena Karumarot <shamseenakarumarot@gmail.com>
2020-04-21 20:11:51 +0200

    Deletetodo function done

--- todos-app/src/App.js
+++ todos-app/src/App.js
@@ -12,12 +12,18 @@ class App extends Component {
   }
   deleteTodo = (id) => {
     console.log(id);
+    const newlist = this.state.todos.filter(todo => {return todo.id!==id})
+    this.setState({
+      todos : newlist
+    })
   }
   render(){
     return (
-      <div className="center container blue-grey center">
-        <h2 className="center card-action"> Todo-list Managing App</h2>
+      <div className="center container center">
+        <div className="card blue-grey">
+        <h2 className="center white-text"> Todo-list Managing App</h2>
         <Todos todos={this.state.todos} deleteTodo={this.deleteTodo}/>
+        </div>
       </div>
     );
     }
