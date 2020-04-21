ab28a11b8e7492eeded4ed1ef9948d73b17e2b65
Shamseena Karumarot <shamseenakarumarot@gmail.com>
2020-04-21 21:34:55 +0200

    setstate of Addtodo added on input change

--- todos-app/src/AddTodo.js
+++ todos-app/src/AddTodo.js
@@ -4,14 +4,24 @@ class AddTodo extends Component{
     state = {
         newtodo : ''
     }
+    handleChange = (e) =>{
+        this.setState({
+            newtodo: e.target.value
+        })
+        console.log(e.target.value)
+    }
+    handleSubmit = (e) =>{
+        e.preventDefault();
+        
+    }
     render(){
     return (
            <div className="card">
             <div className="card-content">
                 <h5 className="blue-text">Add New Todo Tasks:</h5>
-                <form>                           
-                    <input type="text" placeholder="Add New Todo"></input>
-                    <button class="btn waves-effect waves-light" type="submit">Click to Add</button>
+                <form onSubmit={this.handleSubmit}>                           
+                    <input type="text" placeholder="Add New Todo" onChange={this.handleChange}></input>
+                    <button className="btn waves-effect waves-light" type="submit">Click to Add</button>
                 </form>
                 </div>
            </div>
